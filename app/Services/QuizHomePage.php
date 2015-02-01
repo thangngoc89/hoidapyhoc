c007d1a8571bc74d872ad238ba8e20ae1f0e22ad
thangngoc89 <khoa02031995@gmail.com>
2015-02-01 16:12:30 +0700

    Refactor QuizHomePage

--- app/Services/QuizHomePage.php
+++ app/Services/QuizHomePage.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Contracts\Auth\Guard;
 use Quiz\lib\Repositories\Exam\ExamRepository;
+use Illuminate\Cache\Repository as Cache;
 
 class QuizHomePage {
     /**
@@ -13,43 +14,98 @@ class QuizHomePage {
      */
     private $test;
 
+    private $doneTestId;
+
+    private $request;
+
+    private $result;
+
+    private $name;
+    /**
+     * @var Cache
+     */
+    private $cache;
+
     /**
      * @param Guard $auth
      * @param ExamRepository $test
+     * @param Cache $cache
      */
-    public function __construct(Guard $auth, ExamRepository $test)
+    public function __construct(Guard $auth, ExamRepository $test, Cache $cache)
     {
         $this->auth = $auth;
         $this->test = $test;
+        $this->cache = $cache;
     }
     public function execute($request)
     {
-        switch($request->tab)
-        {
-            case 'done' :
-                if (! $this->auth->check())
-                    return redirect('quiz');
-                $tests = $this->test->doneTest($this->auth->user());
-                $name = 'Các đề bạn đã làm';
-                break;
-            case null :
-                $tests = $this->test->orderBy('tests.created_at','DESC');
-                $name = 'Quiz';
-                break;
-            default :
-                return redirect('quiz');
-        }
+        $key = $request->url().$request->tab.$request->page;
 
-        $doneTestId = ($this->auth->check()) ? $this->test->doneTestId($this->auth->user()) : false;
+        $cache = $this->cache->tags('index','tests');
 
-        $key = $request->url().$request->page;
+        if ($cache->has($key))
+            return $cache->get($key);
 
-        $tests = \Cache::tags('tests','index')->remember($key, 10, function() use ($tests)
-        {
-            return $tests->has('question')->with('tagged','user')->paginate(10);
-        });
-        $tests->appends($request->only('tab'));
+        $this->request = $request;
 
-        return view('quiz.index',compact('tests','name','doneTestId'));
+        $tab = $this->switchMethod();
+
+        $this->{$tab}();
+
+        $this->setDoneTestId();
+
+        $view = $this->makeView();
+
+        $cache->put($key,$view,20);
+
+        return $view;
+    }
+
+    private function doneTab()
+    {
+        if (! $this->auth->check())
+            abort(503);
+        $this->result = $this->test->doneTest($this->auth->user());
+
+        $this->name = 'Các đề bạn đã làm';
+    }
+
+    private function latestTab()
+    {
+        $this->result = $this->test->orderBy('tests.created_at','DESC');
+        $this->name = 'Quiz';
+    }
+    /**
+     * @param mixed $doneTestId
+     */
+    public function setDoneTestId()
+    {
+        $this->doneTestId = ($this->auth->check()) ? $this->test->doneTestId($this->auth->user()) : false;
+    }
+
+    private function switchMethod()
+    {
+        $tab = $this->request->tab;
+
+        if (!$tab)
+            $tab = 'latest';
+        if (!in_array($tab, ['done','latest']))
+            abort(404);
+
+        $tab .= 'Tab';
+
+        return $tab;
+    }
+
+    private function makeView()
+    {
+        $tests = $this->result->has('question')->with('tagged','user')->paginate(10);;
+
+        $tests->appends($this->request->only('tab'));
+
+        $name  = $this->name;
+        $doneTestId = $this->doneTestId;
+
+        return view('quiz.index',compact('tests','name','doneTestId'))->render();
     }
 } 

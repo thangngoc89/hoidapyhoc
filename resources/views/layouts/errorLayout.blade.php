0f361180e626cb172987a756f55c5fc2315381a6
thangngoc89 <khoa02031995@gmail.com>
2015-02-20 02:09:07 +0700

    Edit error.css in layout

--- resources/views/layouts/errorLayout.blade.php
+++ resources/views/layouts/errorLayout.blade.php
@@ -4,7 +4,7 @@
 <title>{{ $status }} - Hỏi Đáp Y Học</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<link href="/css/error.css" rel="stylesheet" type="text/css" media="all" />
+<link href="/assets/css/error.css" rel="stylesheet" type="text/css" media="all" />
 </head>
 <body>
 	<!-----start-wrap--------->

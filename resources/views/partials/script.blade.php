f0b76f9a14eab03e523db32ecf0a1d28f78be7a4
thangngoc89 <khoa02031995@gmail.com>
2015-02-20 16:37:44 +0700

    Google Analytics embed

--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -22,18 +22,7 @@
     </div>
 </div>
 @endif
-<!-- Social Embed -->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&appId=383096061852800&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<script src="https://apis.google.com/js/platform.js" async defer>
-  {lang: 'vi'}
-</script>
+<!-- Main file -->
 <script src="{{ elixir('assets/js/vendor.js') }}"></script>
 <script src="{{ elixir('assets/js/script.js') }}"></script>
 <script>new WOW().init();</script>
@@ -59,4 +48,30 @@
     @endforeach
 @endif
 </script>
+<!-- Social Embed -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&appId=383096061852800&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<script src="https://apis.google.com/js/platform.js" async defer>
+  {lang: 'vi'}
+</script>
+<!-- GA -->
+<script type="text/javascript">
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','__gaTracker');
+
+    __gaTracker('create', 'UA-57021343-1', 'auto');
+    __gaTracker('set', 'forceSSL', true);
+    __gaTracker('require', 'displayfeatures');
+    __gaTracker('send','pageview');
+
+</script>
 @yield('script')
+

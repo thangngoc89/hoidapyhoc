9a5c1c3979d65e9c0e3420ccd5854e7e33afa35b
thangngoc89 <khoa02031995@gmail.com>
2015-02-23 18:01:17 +0700

    Init Parse with input tags

--- app/Services/Leecher/CSYK/GetQuiz.php
+++ app/Services/Leecher/CSYK/GetQuiz.php
@@ -6,8 +6,21 @@ use Quiz\lib\Helpers\Str;
 
 class GetQuiz extends BaseLeecher {
 
+
+    const POS_FIRST_ANS = 1;
+
+    const POS_MIDDLE_ANS = 2;
+
+    const POS_LAST_ANS = 3;
+
     private $body;
 
+    /**
+     * Simple get HTML Body of given link
+     *
+     * @param $link
+     * @return $this
+     */
     public function get($link)
     {
         $response = $this->client->get($link);
@@ -17,6 +30,10 @@ class GetQuiz extends BaseLeecher {
         return $this;
     }
 
+    /**
+     * Main function : parse information
+     *
+     */
     public function parse()
     {
         $html = $this->parser->str_get_html($this->body);
@@ -26,6 +43,11 @@ class GetQuiz extends BaseLeecher {
         return $quiz;
     }
 
+    /**
+     * Parse into quiz structure
+     *
+     * @param $html
+     */
     private function parseQuiz($html)
     {
         $name = $this->parseName($html);
@@ -35,6 +57,12 @@ class GetQuiz extends BaseLeecher {
         $questions = $this->breakIntoQuestions($content);
     }
 
+    /**
+     * Parse exam's name (post's title)
+     *
+     * @param $html
+     * @return string
+     */
     private function parseName($html)
     {
         $name = $html->find('div[class=page_heading]',0)->plaintext;
@@ -47,37 +75,60 @@ class GetQuiz extends BaseLeecher {
     {
         $questions = [];
         $item = [];
-        foreach ($content->find('p') as $row)
+        $inputs = $content->find('input[onclick^="alert"]');
+
+        $input = $inputs[0];
+
+        foreach ($inputs as $input)
         {
-            if (trim($row->plaintext) == '&nbsp;' || $row->outertext == '<hr>')
+            $cursor = $this->cursorPosition($input);
+            if ($cursor == self::POS_FIRST_ANS)
             {
-                $questions[] = $item;
-                $item = [];
-            } else {
-                $item[] = $row;
-            }
-        }
-
-        $questions = array_filter($questions);
-
-        return $this->previewQuestionsArray($questions);
-    }
-
-    private function previewQuestionsArray($questions)
-    {
-        foreach ($questions as $q)
-        {
-            foreach ($q as $row)
-            {
-                echo $row->plaintext."<br>";
+                $item[] = $this->getQuestionSentence($input);
+                $item[] = $this->parseHintAndValueFromInut($input->outertext);
             }
 
-            echo '<hr>';
+            if ($cursor == self::POS_MIDDLE_ANS)
+                $item[] = $this->parseHintAndValueFromInut($input->outertext);
         }
-    }
-    private function serializeAnswerRow($html)
-    {
+
+//        $questions = array_filter($questions);
 
     }
+
+
+    private function cursorPosition($input)
+    {
+        $parent = $input->parent()->parent();
+
+        $next_sib = $parent->next_sibling();
+        $prev_sib = $parent->prev_sibling();
+
+        if (is_null($prev_sib->find('input[onclick]')))
+            return self::POS_FIRST_ANS;
+
+        if (!is_null($next_sib)->find('input[onclick]'))
+            return self::POS_MIDDLE_ANS;
+    }
+
+    private function getQuestionSentence($input)
+    {
+        $sentence = $input->parent()->parent()->prev_sibling()->plaintext;
+        return trim($sentence);
+    }
+
+
+    private function parseHintAndValueFromInut($input)
+    {
+        preg_match('/&quot;(.*?)&quot;/i', $input, $hint);
+        preg_match('/value=\"(.*?)\"/i', $input, $value);
+
+        return [
+            'value' => $value[1],
+            'hint' => $hint[1],
+        ];
+    }
+
+
 
 } 

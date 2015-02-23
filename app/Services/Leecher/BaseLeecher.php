83a844945b60282d9307f2d3ac00c974f4c11dfc
thangngoc89 <khoa02031995@gmail.com>
2015-02-23 13:11:39 +0700

    Base on p tag (unsuccess)

---
+++ app/Services/Leecher/BaseLeecher.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dang
+ * Date: 23/02/2015
+ * Time: 11:04 AM
+ */
+
+namespace Services\Leecher;
+
+
+class BaseLeecher {
+
+} 
---
+++ app/Services/Leecher/CSYK/GetQuiz.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dang
+ * Date: 23/02/2015
+ * Time: 11:11 AM
+ */
+
+namespace Services\Leecher\CSYK;
+
+
+class GetQuiz {
+
+} 

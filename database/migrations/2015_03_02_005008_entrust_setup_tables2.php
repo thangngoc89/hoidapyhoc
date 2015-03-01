90b98dae4f596e6f396e0713efdaf34c0d987ae0
thangngoc89 <khoa02031995@gmail.com>
2015-03-02 02:33:54 +0700

    Fix duplicate class

--- database/migrations/2015_03_02_005008_entrust_setup_tables2.php
+++ database/migrations/2015_03_02_005008_entrust_setup_tables2.php
@@ -2,7 +2,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class EntrustSetupTables extends Migration {
+class EntrustSetupTables2 extends Migration {
     /** * Run the migrations. * * @return void */
 
     public function up() {

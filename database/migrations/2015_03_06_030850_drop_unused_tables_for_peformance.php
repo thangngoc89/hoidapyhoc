99b6677e20f01d9db97cf1867b609c1248a50dd5
thangngoc89 <khoa02031995@gmail.com>
2015-03-06 03:16:55 +0700

    Drop 4 unused tables

---
+++ database/migrations/2015_03_06_030850_drop_unused_tables_for_peformance.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropUnusedTablesForPeformance extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        DB::transaction(function()
+        {
+            Schema::table('exams', function($table)
+            {
+                $table->dropForeign('tests_cid_foreign'); // Drop foreign key 'user_id' from 'posts' table tests_cid_foreign
+            });
+            Schema::drop('categories');
+            Schema::drop('comments');
+            Schema::drop('posts');
+            Schema::drop('question');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
